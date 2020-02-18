7ae122a505a01f2691aa4bf36d16e029fe5038d8
rdcs <riosdcs@gmail.com>
2020-02-17 23:54:09 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const Label = ({ name }) => {
+  return (
+    <label for="name">credit card number:</label>
+  );
+}
+
+const Input = ({ name, id, placeholder }) => {
+  return (
+    <>
+      <Label for="name" />
+      <input name={name} id={id} placeholder={placeholder}></input>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Input name="cc-number" id="cc-number" placeholder="enter credit card number" />
     </div>
   );
 }
