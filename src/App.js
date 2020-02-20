63b8907d82aea0311492abef54c1a7e3a2e712c9
rdcs <riosdcs@gmail.com>
2020-02-20 02:21:04 -0800

    Implement way too many things for one commit
    I'm done for the night. I'll do better commits.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
 import React from 'react';
+import visa from './assets/visa.png';
+import mastercard from './assets/mastercard.png';
+import discover from './assets/discover.png';
+import amex from './assets/amex.png';
 
-const Label = ({ name }) => {
-  return (
-    <label for="name">credit card number:</label>
-  );
-}
+const prefix = new Map([
+  ['cc-visa', '4'],
+  ['cc-mastercard', ['51', '52', '53', '54', '55']],
+  ['cc-discover', ['6011', '64', '65']],
+  ['cc-amex', ['34', '37']],
+]);
 
-const Input = ({ name, id, placeholder }) => {
+const Logo = ({ type, alt, imgClass }) => {
   return (
     <>
-      <Label for="name" />
-      <input name={name} id={id} placeholder={placeholder}></input>
+      <img src={type} alt={`${alt} credit card logo`} className={imgClass} />
     </>
   );
 }
 
+class CreditCardForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      maxLength: 19,
+      cardNumber: '',
+      placeholder: 'Enter credit card number',
+      active: 'cc-logo inactive',
+      type: '',
+    };
+  }
+
+  determineType = (cardNumber) => {
+    prefix.forEach( (value, key) => {
+      if (cardNumber.startsWith(key)) {
+        console.log("hey, keys match");
+      }
+    });
+  }
+
+  insertSpace = (cardNumber) => {
+    /* TODO: Group digits visually and appropriately. */
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.cardNumber !== this.state.cardNumber) {
+      this.insertSpace(this.state.cardNumber);
+      this.determineType(this.state.cardNumber);
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      cardNumber: e.target.value
+    });
+  }
+
+  handleClick = (e) => {
+    this.setState({
+      cardNumber: '',
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <div className="input-addon">
+        <input type="text"
+          value={this.state.cardNumber}
+          placeholder={this.state.placeholder}
+          maxLength={this.state.maxLength}
+          onChange={this.handleChange}
+        />
+        <button className="reset" onClick={this.handleClick}>reset</button>
+        </div>
+        <div>
+          <Logo type={visa} 
+            alt="Visa" 
+            imgClass={this.state.active} /* move to function */
+          />
+          <Logo type={mastercard} 
+            alt= "Mastercard" 
+            imgClass={this.state.active}
+          />
+          <Logo type={discover}
+            alt="Discover" 
+            imgClass={this.state.active} 
+          />
+          <Logo type={amex}
+            alt="American Express" 
+            imgClass={this.state.active}
+          />
+        </div>
+      </>
+    );
+  }
+}
+
 function App() {
   return (
-    <div>
-      <Input name="cc-number" id="cc-number" placeholder="enter credit card number" />
+    <div className="cc-form">
+      <CreditCardForm />
     </div>
   );
 }
